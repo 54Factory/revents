135cd9ae257b3b657737de07618839bcdc1aa408
Lorenz Coates <lorenz@Lorenzs-MacBook-Pro.local>
2018-09-22 08:19:08 -0400

    Forms 101 - End of Section 4

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import cuid from 'cuid';
 import { Grid, Button } from 'semantic-ui-react';
 import EventList from '../EventList/EventList'
 import EventForm from '../EventForm/EventForm'
@@ -79,6 +80,16 @@ class EventDashboard extends Component {
     })
   }
 
+  handleCreateEvent = (newEvent) => {
+    newEvent.id = cuid();
+    newEvent.hostPhotoURL = '/assets/user.png';
+    const updatedEvents = [...this.state.events, newEvent]
+    this.setState({
+      events: updatedEvents,
+      isOpen: false
+    })
+  }
+
   render() {
     return (
       <div>
@@ -88,7 +99,7 @@ class EventDashboard extends Component {
           </Grid.Column>
           <Grid.Column width={6}>
             <Button onClick={this.handleFormOpen} positive content="Create Event" />
-              {this.state.isOpen && <EventForm handleCancel={this.handleCancel} />}
+              {this.state.isOpen && <EventForm createEvent={this.handleCreateEvent} handleCancel={this.handleCancel} />}
           </Grid.Column>
         </Grid>
       </div>
