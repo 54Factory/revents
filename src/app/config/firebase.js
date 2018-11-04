8718fd07b9fe5ec65d85b7412116f143c9a2e195
Lorenz Coates <lorenz@Lorenzs-MBP.fios-router.home>
2018-11-04 07:17:45 -0500

    Firestore Implementation

---
+++ src/app/config/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDqU2jCLNu7ZBK9Gi7vXqtuUzCsbNQJLyM",
+  authDomain: "revents-218808.firebaseapp.com",
+  databaseURL: "https://revents-218808.firebaseio.com",
+  projectId: "revents-218808",
+  storageBucket: "",
+  messagingSenderId: "419028950071"
+}
+
+firebase.initializeApp(firebaseConfig);
+const firestore = firebase.firestore();
+const settings = {
+  timestampsInSnapshots: true
+}
+
+firestore.settings(settings)
+export default firebase;
