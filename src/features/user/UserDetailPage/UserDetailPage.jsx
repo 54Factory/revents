d8b66445b3b60aae8ae3fb14a1280c327ab5f008
Lorenz Coates <lorenz@Lorenzs-MacBook-Pro.local>
2018-11-08 10:42:06 -0500

    Paging, Sorting

--- src/features/user/UserDetailPage/UserDetailPage.jsx
+++ src/features/user/UserDetailPage/UserDetailPage.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { firestoreConnect, isEmpty } from 'react-redux-firebase';
 import { compose } from 'redux';
@@ -6,53 +6,67 @@ import { Grid } from "semantic-ui-react";
 import UserDetailHeader from './UserDetailHeader'
 import UserDetailDescription from './UserDetailDescription';
 import UserDetailPhotos from './UserDetailPhotos';
-import UserDetailedSidebar from './UserDetailSidebar';
+import UserDetailSidebar from './UserDetailSidebar';
 import UserDetailEvents from './UserDetailEvents';
 import LoadingComponent from '../../../app/layout/LoadingComponent'
 import { userDetailQuery } from '../userQueries'
+import { getUserEvents } from '../userActions';
 
 const mapState = (state, ownProps) => {
   let userUid = null;
-  let profile = {}
+  let profile = {};
+
   if (ownProps.match.params.id === state.auth.uid) {
     profile = state.firebase.profile
   } else {
     profile = !isEmpty(state.firestore.ordered.profile) && state.firestore.ordered.profile[0];
-    userUid = ownProps.match.params.id
+    userUid = ownProps.match.params.id;
   }
   return {
     profile,
     userUid,
+    events: state.events,
+    eventsLoading: state.async.loading,
     auth: state.firebase.auth,
     photos: state.firestore.ordered.photos,
     requesting: state.firestore.status.requesting
   }
 }
 
+const actions = {
+  getUserEvents
+}
 class UserDetailPage extends Component {
-  
+
+  async componentDidMount() {
+    let events = await this.props.getUserEvents(this.props.userUid);
+    console.log(events);
+  }
+
+  changeTab = (e, data) => {
+    this.props.getUserEvents(this.props.userUid, data.activeIndex)
+  }
+
   render() {
-    const { profile, photos, auth, match, requesting } = this.props;
+    const {profile, photos, auth, match, requesting, events, eventsLoading} = this.props;
     const isCurrentUser = auth.uid === match.params.id;
-    const loading = Object.values(requesting).some(a => a === true)
+    const loading = Object.values(requesting).some(a => a === true);
 
-    if (loading) return <LoadingComponent inverted={true} />
-
+    if (loading) return <LoadingComponent inverted={true}/>
     return (
       <Grid>
-        <UserDetailHeader profile={profile} />
-        <UserDetailDescription profile={profile} />
-        <UserDetailedSidebar isCurrentUser={isCurrentUser} />
-      {photos && photos.length > 0 &&
-        <UserDetailPhotos photos={photos} />
-      }
-        <UserDetailEvents />
+        <UserDetailHeader profile={profile}/>
+        <UserDetailDescription profile={profile}/>
+        <UserDetailSidebar isCurrentUser={isCurrentUser}/>
+        {photos && photos.length > 0 &&
+        <UserDetailPhotos photos={photos}/>}
+        <UserDetailEvents changeTab={this.changeTab} events={events} eventsLoading={eventsLoading}/>
       </Grid>
     );
   }
 }
 
 export default compose(
-  connect(mapState),
+  connect(mapState, actions),
   firestoreConnect((auth, userUid) => userDetailQuery(auth, userUid))
   )(UserDetailPage);
